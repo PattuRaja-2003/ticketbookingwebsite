d31940ed9180d2832f2587351b37bbeabf72035a
patturaja501 <patturaja501@gmail.com>
2026-03-19 12:14:27 +0530

    cors origin upadated

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-const express=require('express')
-const mongoose=require('mongoose')
-const dotenv=require('dotenv');
-const userRouter = require('./router/userRouter');
-const moviesRouter=require('./router/moviesRouter')
-const bookingRouter=require('./router/bookingRouter')
-const cors=require('cors')
-const app=express()
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRouter = require("./router/userRouter");
+const moviesRouter = require("./router/moviesRouter");
+const bookingRouter = require("./router/bookingRouter");
+const cors = require("cors");
+const app = express();
 dotenv.config();
-app.use(express.json())
-const url= process.env.DATABASE_URL;
-mongoose.connect(url).then(()=>{
-    console.log("Data Base is connected")
-})
-corsOption={
-    origin:'https://ticketbookingwebsite-frontent.vercel.app/'
-}
-app.use(cors(corsOption))
-app.use('/user',userRouter)
-app.use('/movies',moviesRouter)
-app.use('/booking',bookingRouter)
+app.use(express.json());
+const url = process.env.DATABASE_URL;
+mongoose.connect(url).then(() => {
+  console.log("Data Base is connected");
+});
+corsOption = {
+  origin: "https://ticketbookingwebsite-frontent.vercel.app",
+};
+app.use(cors(corsOption));
+app.use("/user", userRouter);
+app.use("/movies", moviesRouter);
+app.use("/booking", bookingRouter);
 
-app.listen(3001,()=>{
-    console.log("Server is Connected")
-})
-
+app.listen(3001, () => {
+  console.log("Server is Connected");
+});
